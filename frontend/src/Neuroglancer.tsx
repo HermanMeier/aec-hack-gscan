8b59e2f423d77ff9638b963a64e86de35ea83963
Hendrig Sellik <hendrig.sellik@gscan.eu>
2024-11-23 17:11:24 +0200

    Commented code to change default behaviour of Neuroglancer

--- frontend/src/Neuroglancer.tsx
+++ frontend/src/Neuroglancer.tsx
@@ -1,8 +1,16 @@
 import './App.css'
 import "neuroglancer";
 import {setupDefaultViewer} from "neuroglancer/unstable/ui/default_viewer_setup.js";
+// import {DataSourceProvider} from "neuroglancer/unstable/datasource";
 
 function App() {
+    // const datasourceProvider = {normalizeUrl: 'precomputed://http://138.246.22.182:8081'}
+    // const dataSources: Map<string, DataSourceProvider> = new Map(['img', datasourceProvider])
+    // setupDefaultViewer( {
+    //     dataSourceProvider: {
+    //         dataSources: {'img' {normalizeUrl: 'precomputed://http://138.246.22.182:8081'}}
+    //     }
+    // });
     setupDefaultViewer();
     return (
         <>
