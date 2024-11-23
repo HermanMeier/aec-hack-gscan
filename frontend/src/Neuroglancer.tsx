f79f878b5e18cc39e530aa20d707ae283ef036d2
Hendrig Sellik <hendrig.sellik@gscan.eu>
2024-11-23 14:50:11 +0200

    feat: Add Neuroglancer window

---
+++ frontend/src/Neuroglancer.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import "neuroglancer";
+import {setupDefaultViewer} from "neuroglancer/unstable/ui/default_viewer_setup.js";
+
+function App() {
+    setupDefaultViewer();
+    return (
+        <>
+        </>
+    )
+}
+
+export default App
