3cef6ce4194a94fb17ec7df2536fbdcefa74062c
Hendrig Sellik <hendrig.sellik@gscan.eu>
2024-11-23 18:11:13 +0200

    fix: Fix vite config to build Neuroglancer correctly

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,23 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  worker: {
+    // Required due to use of dynamic imports in Neuroglancer.
+    format: "es",
+  },
+  build: {
+    assetsDir: "",
+    rollupOptions: {
+      output: {
+        format: "esm",
+        assetFileNames: (assetInfo) => {
+          const { name } = assetInfo;
+          if (name?.endsWith(".html")) {
+            return "[name][extname]";
+          }
+          return "[name]-[hash][extname]";
+        },
+      },
+    },
+  },
 })
